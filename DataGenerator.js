e63ccd091b400f1ac1ff1e2518dd0a3b2198d1b4
sghongadi@gmail.com <sghongadi@gmail.com>
2021-07-25 21:05:29 +0100

    generates data at every 5 secs

--- DataGenerator.js
+++ DataGenerator.js
@@ -1,17 +1,39 @@
-//Function to generate random number
-function getRandomBetween(min, max) {
+
+const csv = require('csv-parser');
+const fs = require('fs');
+
+//console.log(newString);
+
+
+function generateData() {
+
+  //=================Function to generate random number======================
+  function getRandomBetween(min, max) {
     return Math.random() * (max - min) + min;
   }
 
   const currentDate = new Date();
-
   const currentDayOfMonth = currentDate.getDate();
-  const currentMonth = currentDate.getMonth(); // Be careful! January is 0, not 1
+  const currentMonth = currentDate.getMonth();
   const currentYear = currentDate.getFullYear();
-  
-  const dateString = currentDayOfMonth + "-" + (currentMonth + 1) + "-" + currentYear;
 
-//Returns a random number between 0 and 100
-console.log(getRandomBetween(0,100));
-console.log(dateString);
-console.log(getRandomBetween(0,100) + " generated at " + dateString)
+  var today = new Date();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+  //===================Concat the time and date ==========================================
+  const dateString = currentDayOfMonth + "-" + (currentMonth + 1) + "-" + currentYear + " Current Time: " + time;
+
+ //===================Returns a random number between 0 and 100==================
+  //console.log(getRandomBetween(0,100));
+  //console.log(dateString);
+  const newString = getRandomBetween(0, 100) + " generated at " + dateString + "\n"
+
+  //fs.writeFileSync("data.csv", newString);
+  //=================Append data to CSV file======================================
+  fs.appendFileSync("data.csv", newString);
+}
+
+let display = setInterval(generateData, 5000);
+
+
+
